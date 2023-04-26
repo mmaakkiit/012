2a3efe2ef9c83ded8edff0924fb5d9410abe9054
mmaakkiit <109716558+mmaakkiit@users.noreply.github.com>
2023-04-26 18:55:40 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,8 @@
 input.onButtonPressed(Button.A, function () {
-    最初の明るさ = input.lightLevel()
     停止 = 0
+})
+input.onGesture(Gesture.Shake, function () {
+    停止 = 1
 })
 input.onButtonPressed(Button.AB, function () {
     秒数 = 0
@@ -9,24 +11,14 @@ input.onButtonPressed(Button.AB, function () {
     basic.clearScreen()
     basic.showNumber(秒数)
 })
-input.onButtonPressed(Button.B, function () {
-    停止 = 1
-})
-let 最初の明るさ = 0
 let 停止 = 0
 let 秒数 = 0
 秒数 = 0
 停止 = 1
-basic.showString("Hello!")
-basic.clearScreen()
-basic.showString("Ready!")
-basic.clearScreen()
-if (停止 == 0) {
-	
-}
 basic.showNumber(秒数)
 basic.forever(function () {
     while (停止 == 0) {
+        let 最初の明るさ = 0
         if (input.lightLevel() < 最初の明るさ - 20) {
             停止 = 1
         } else {
